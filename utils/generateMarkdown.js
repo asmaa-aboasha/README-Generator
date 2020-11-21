60bf7c271eb9a21b7ad3cf79b56a614d9ec3e9fa
Asmaa <asmaa.aboasha@gmail.com>
2020-11-21 04:21:44 -0800

    Added function to generate markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,37 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contributing 
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## License
+  ${data.license}
+
+  ## Questions
+  For questions, please contact me at: 
+  GitHub: https://github.com/${data.github}
+  Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
